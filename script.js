a3b3ae6a75e41625cbd5c157c602904a6547e899
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-18 09:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+	domLoadSuccess();
+});
+
+function domLoadSuccess() {
+	const bodyElement = document.body;
+	const paragraph = document.createElement('p');
+	paragraph.innerText = 'DOM load success';
+	bodyElement.appendChild(paragraph);
+}
